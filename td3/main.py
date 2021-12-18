24ca6d3636abe99c76b8f1c0dd5863cc8cec1b1a
ywfu <royxroy@163.com>
2021-12-18 10:18:13 -0500

    fix typo

--- td3/main.py
+++ td3/main.py
@@ -47,7 +47,7 @@ def get_args():
     parser.add_argument("--noise_clip", default=0.5, type=float)
     parser.add_argument("--policy_freq", default=2, type=int)
     parser.add_argument("--log_dir", default="./logs", type=str)
-    parser.add_argument("--model_dir", default="./models", type=str)
+    parser.add_argument("--model_dir", default="./saved_models", type=str)
     args = parser.parse_args()
     return args
 
