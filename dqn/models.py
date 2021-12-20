fe138ecf76a8f336d3cf77a072037f067d4398fd
fuyw <royxroy@163.com>
2021-12-20 00:56:33 -0500

    dqn

---
+++ dqn/models.py
@@ -0,0 +1,91 @@
+import functools
+
+from flax import linen as nn
+from flax import serialization
+from flax.training import train_state
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+
+
+class QNet(nn.Module):
+    act_dim: int
+
+    def setup(self):
+        self.l1 = nn.Dense(64, name="fc1")
+        self.l2 = nn.Dense(64, name="fc2")
+        self.l3 = nn.Dense(self.act_dim, name="fc3")
+
+    def __call__(self, inputs: jnp.ndarray):
+        x = nn.relu(self.l1(inputs))
+        x = nn.relu(self.l2(x))
+        q_values = self.l3(x)
+        return q_values
+
+    def Repr(self, inputs: jnp.ndarray):
+        x = nn.relu(self.l1(inputs))
+        x = nn.relu(self.l2(x))
+        q_values = self.l3(x)
+        return x, q_values
+
+
+class DQN:
+    def __init__(self, obs_dim, act_dim, learning_rate, gamma, seed, target_update_period):
+        
+        self.gamma = gamma
+        self.target_update_period = target_update_period
+
+        # Dummy Inputs
+        dummy_obs = jnp.ones([1, obs_dim], dtype=jnp.float32)
+
+        self.qnet = QNet(act_dim)
+        self.pred = jax.jit(self.qnet.apply)
+        params = self.qnet.init(jax.random.PRNGKey(seed), dummy_obs)["params"]
+        self.target_params = params
+        self.state = train_state.TrainState.create(
+            apply_fn=QNet.apply,
+            params=params,
+            tx=optax.adam(learning_rate=learning_rate))
+        self.update_step = 0
+
+    @functools.partial(jax.jit, static_argnames=("self"))
+    def train_step(self, batch, state, target_params):
+        next_q = jnp.max(self.pred({"params": target_params}, batch.next_observations),
+                         axis=-1, keepdims=True)
+        target_q = batch.rewards + self.gamma * batch.discounts * next_q
+
+        def q_learning(qs, action, target_q):
+            return (qs[action] - target_q)**2, qs[action]
+
+        def loss_fn(params, batch):
+            qs = self.qnet.apply({"params": params}, batch.observations)
+            q_loss, q = jax.vmap(q_learning)(qs, batch.actions, target_q)
+            q_loss = q_loss.mean()
+            return q_loss, {"q_loss": q_loss, "q": q.mean()}
+        
+        (q_loss, log_info), q_grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params, batch)
+        state = state.apply_gradients(grads=q_grads)
+        return log_info, state
+
+    def select_action(self, params, observations):
+        observations = jax.device_put(observations[None, ...])
+        q_values = self.pred({"params": params}, observations)
+        action = np.random.choice(np.flatnonzero(q_values == q_values.max()))
+        return int(action)
+
+    def train(self, replay_buffer, batch_size):
+        self.update_step += 1
+        batch = replay_buffer.sample(batch_size)
+        log_info, self.state = self.train_step(batch, self.state, self.target_params)
+
+        # update the taret network
+        if self.update_step % self.target_update_period:
+            self.target_params = self.state.params
+    
+        return log_info
+
+    def save(self, filename):
+        model_file = filename + "_qnet.ckpt"
+        with open(model_file, "wb") as f:
+            f.write(serialization.to_bytes(self.state.params))
---
+++ dqn/test.py
@@ -0,0 +1,58 @@
+import bsuite
+from bsuite import sweep
+from bsuite.utils import gym_wrapper
+import numpy as np
+
+from models import QNet, DQN
+from utils import ReplayBuffer, evaluate_catch
+
+
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default="catch/0")
+    parser.add_argument("--seed", default=0, type=int)
+    parser.add_argument("--num_episodes", default=100, type=int)
+    parser.add_argument("--start_episodes", default=10, type=int)
+    parser.add_argument("--eval_episodes", default=5, type=int)
+    parser.add_argument("--epsilon", default=0.05, type=float)
+    parser.add_argument("--gamma", default=0.99, type=float)
+    parser.add_argument("--batch_size", default=64, type=float)
+    parser.add_argument("--learning_rate", default=1e-3, type=float)
+    parser.add_argument("--target_update_period", default=4, type=int)
+    args = parser.parse_args()
+    return args
+
+
+args = get_args()
+sweep.SETTINGS["catch/0"]["seed"] = args.seed
+bsuite_env = bsuite.load_from_id(bsuite_id=args.env)
+env = gym_wrapper.GymFromDMEnv(bsuite_env)
+np.random.seed(args.seed)
+
+obs_dim = np.prod(env.observation_space.shape)
+act_dim = env.action_space.n
+print(f"Run DQN on Env {args.env}: obs_dim = {obs_dim}, act_dim = {act_dim}")
+agent = DQN(obs_dim, act_dim, args.learning_rate, args.gamma,
+            args.seed, args.target_update_period)
+replay_buffer = ReplayBuffer(obs_dim=obs_dim)
+
+for episode in range(args.num_episodes):
+    obs, done = env.reset(), False
+    while not done:
+        if np.random.random() < args.epsilon:
+            action = np.random.randint(0, 3)
+        else:
+            action = agent.select_action(agent.state.params, obs.flatten())
+        next_obs, reward, done, _ = env.step(action)
+        replay_buffer.add(obs, action, next_obs, reward, float(done))
+        obs = next_obs
+        if replay_buffer.size >= 100:
+            log_info = agent.train(replay_buffer, args.batch_size)
+
+    if (episode + 1) % args.eval_episodes == 0:
+        eval_reward = evaluate_catch(agent)
+        if episode >= args.start_episodes:
+            print(f"# Episode {episode + 1}: reward = {eval_reward:.2f}, "
+                f"q_loss = {log_info['q_loss']:.2f}, q = {log_info['q']:.2f}")
+
