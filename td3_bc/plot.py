725997263ce97c126d802be9c633f12470179484
ywfu <royxroy@163.com>
2021-12-21 10:19:16 -0500

    plot

--- td3_bc/plot.py
+++ td3_bc/plot.py
@@ -2,12 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-env = 'halfcheetah-random-v0'
-seed = 0
-df = pd.read_csv(f'/usr/local/data/yuweifu/jaxrl/td3_bc/td3_logs/td3bc_{env}/{seed}.csv', index_col=0)
-x_np = np.load(f'/usr/local/data/yuweifu/jaxrl/td3_bc/TD3_BC/results/TD3_BC_{env}_{seed}.npy')
 
-plt.plot(range(200), x_np, label='torch')
-plt.plot(range(200), df['reward'].values[1:], label='jax')
-plt.legend()
+
+envs=[
+    "halfcheetah-random-v0",
+    "hopper-random-v0",
+    "walker2d-random-v0",
+    "halfcheetah-medium-v0",
+    "hopper-medium-v0",
+    "walker2d-medium-v0",
+    "halfcheetah-expert-v0",
+    "hopper-expert-v0",
+    "walker2d-expert-v0",
+    "halfcheetah-medium-expert-v0",
+    "hopper-medium-expert-v0",
+    "walker2d-medium-expert-v0",
+    "halfcheetah-medium-replay-v0",
+    "hopper-medium-replay-v0",
+    "walker2d-medium-replay-v0"
+]
+
+_, axes = plt.subplots(nrows=3, ncols=5, figsize=(20, 12))
+for idx, env in enumerate(envs):
+    ax = axes[idx // 5][idx % 5]
+    seed = 0
+    df = pd.read_csv(f'/usr/local/data/yuweifu/jaxrl/td3_bc/td3_logs/td3bc_{env}/{seed}.csv', index_col=0)
+    x_np = np.load(f'/usr/local/data/yuweifu/jaxrl/td3_bc/TD3_BC/results/TD3_BC_{env}_{seed}.npy')
+    ax.plot(range(200), x_np, label='torch')
+    ax.plot(range(200), df['reward'].values[1:], label='jax')
+    ax.legend(fontsize=4)
+    ax.set_title(env, fontsize=5)
+
+
 plt.savefig('compare.png')
--- td3_bc/run_experiments.sh
+++ td3_bc/run_experiments.sh
@@ -3,24 +3,24 @@
 # Script to reproduce results
 
 envs=(
-	"halfcheetah-random-v0"
-	"hopper-random-v0"
-	"walker2d-random-v0"
+	# "halfcheetah-random-v0"
+	# "hopper-random-v0"
+	# "walker2d-random-v0"
 	"halfcheetah-medium-v0"
 	"hopper-medium-v0"
 	"walker2d-medium-v0"
-	"halfcheetah-expert-v0"
-	"hopper-expert-v0"
-	"walker2d-expert-v0"
-	"halfcheetah-medium-expert-v0"
-	"hopper-medium-expert-v0"
-	"walker2d-medium-expert-v0"
+	# "halfcheetah-expert-v0"
+	# "hopper-expert-v0"
+	# "walker2d-expert-v0"
+	# "halfcheetah-medium-expert-v0"
+	# "hopper-medium-expert-v0"
+	# "walker2d-medium-expert-v0"
 	"halfcheetah-medium-replay-v0"
 	"hopper-medium-replay-v0"
 	"walker2d-medium-replay-v0"
 	)
 
-for ((i=0;i<1;i+=1))
+for ((i=1;i<2;i+=1))
 do 
 	for env in ${envs[*]}
 	do
