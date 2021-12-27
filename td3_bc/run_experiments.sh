aed1a23818738020c2c5eb0dc7bb87d124b92e23
ywfu <royxroy@163.com>
2021-12-27 10:12:11 -0500

    fix offline td3 conflicts

--- td3_bc/run_experiments.sh
+++ td3_bc/run_experiments.sh
@@ -2,25 +2,23 @@
 
 # Script to reproduce results
 
-algo="td3"
-
 envs=(
-	"halfcheetah-random-v0"
+	# "halfcheetah-random-v0"
+	# "hopper-random-v0"
+	# "walker2d-random-v0"
 	"halfcheetah-medium-v0"
-	"halfcheetah-expert-v0"
-	"halfcheetah-medium-expert-v0"
-	"halfcheetah-medium-replay-v0"
-	"hopper-random-v0"
 	"hopper-medium-v0"
-	"hopper-expert-v0"
-	"hopper-medium-expert-v0"
-	"hopper-medium-replay-v0"
-	"walker2d-random-v0"
 	"walker2d-medium-v0"
-	"walker2d-expert-v0"
-	"walker2d-medium-expert-v0"
+	# "halfcheetah-expert-v0"
+	# "hopper-expert-v0"
+	# "walker2d-expert-v0"
+	# "halfcheetah-medium-expert-v0"
+	# "hopper-medium-expert-v0"
+	# "walker2d-medium-expert-v0"
+	"halfcheetah-medium-replay-v0"
+	"hopper-medium-replay-v0"
 	"walker2d-medium-replay-v0"
-)
+	)
 
 for ((i=1;i<2;i+=1))
 do 
@@ -28,7 +26,6 @@ do
 	do
 		python main.py \
 		--env $env \
-		--seed $i \
-        --algo $algo
+		--seed $i
 	done
 done
