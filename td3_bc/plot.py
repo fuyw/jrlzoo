7e9783b92ba26145cd00e2173d945753f6a1c23b
ywfu <royxroy@163.com>
2021-12-27 21:34:10 -0500

    td3bc on d4rl-v2

--- td3_bc/plot.py
+++ td3_bc/plot.py
@@ -27,9 +27,7 @@ for idx, env in enumerate(envs):
     ax = axes[idx // 5][idx % 5]
     seed = 0
     df = pd.read_csv(f'/usr/local/data/yuweifu/jaxrl/td3_bc/td3_logs/td3bc_{env}/{seed}.csv', index_col=0)
-    x_np = np.load(f'/usr/local/data/yuweifu/jaxrl/td3_bc/TD3_BC/results/TD3_BC_{env}_{seed}.npy')
     ax.plot(range(200), x_np, label='torch')
-    ax.plot(range(200), df['reward'].values[1:], label='jax')
     ax.legend(fontsize=4)
     ax.set_title(env, fontsize=5)
 
--- td3_bc/run.py
+++ td3_bc/run.py
@@ -8,15 +8,15 @@ def single_exp(seed='0', env_name="halfcheetah-medium-v0"):
 
 
 def run():
-    tasks = [(i, "halfcheetah-medium-v0") for i in range(0, 5)]
-    threads = []
-    for (seed, env_name) in tasks:
-        t_thread = threading.Thread(target=single_exp, args=(seed, env_name,))
-        t_thread.start()
-        threads.append(t_thread)
-    [t.join() for t in threads]
-    time.sleep(2*3600)
-    tasks = [(i, "hopper-medium-v0") for i in range(0, 5)]
+    # tasks = [(i, "halfcheetah-medium-v0") for i in range(0, 5)]
+    # threads = []
+    # for (seed, env_name) in tasks:
+    #     t_thread = threading.Thread(target=single_exp, args=(seed, env_name,))
+    #     t_thread.start()
+    #     threads.append(t_thread)
+    # [t.join() for t in threads]
+    # time.sleep(2*3600)
+    tasks = [(i, "hopper-medium-v2") for i in range(0, 5)]
     threads = []
     for (seed, env_name) in tasks:
         t_thread = threading.Thread(target=single_exp, args=(seed, env_name,))
