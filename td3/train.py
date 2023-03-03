6dc4b0072a397177601fc4a8fef0e1f22c2fbe17
fuyw <royxroy@163.com>
2023-03-02 19:44:57 -0500

    clean td3

--- td3/train.py
+++ td3/train.py
@@ -5,30 +5,27 @@ import os
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".2"
 
 import ml_collections
+import gym
 import time
-
-import gymnasium as gym
 import numpy as np
 import pandas as pd
 from tqdm import trange
 from models import TD3Agent
 from utils import ReplayBuffer, get_logger
-# from gym_utils import make_env
+from gym_utils import make_env
 
 
 def eval_policy(agent: TD3Agent, env: gym.Env, eval_episodes: int = 10) -> Tuple[float, float]:
     t1 = time.time()
-    avg_reward, avg_step = 0, 0
+    avg_reward = 0.
     for _ in range(eval_episodes):
-        (obs, _), done, truncated = env.reset(), False, False
-        while (not done and not truncated):
-            avg_step += 1
+        obs, done = env.reset(), False
+        while not done:
             action = agent.sample_action(obs)
-            obs, reward, done, truncated, info = env.step(action)  # truncated, info
+            obs, reward, done, _ = env.step(action)
             avg_reward += reward
     avg_reward /= eval_episodes
-    avg_step /= eval_episodes
-    return avg_reward, avg_step, time.time() - t1
+    return avg_reward, time.time() - t1
 
 
 def train_and_evaluate(config: ml_collections.ConfigDict):
@@ -71,7 +68,7 @@ def train_and_evaluate(config: ml_collections.ConfigDict):
     replay_buffer = ReplayBuffer(obs_dim, act_dim)
     logs = [{"step":0, "reward":eval_policy(agent, eval_env, config.eval_episodes)[0]}]
 
-    (obs, _) = env.reset()
+    obs, done = env.reset(), False
     episode_timesteps = 0
     for t in trange(1, config.max_timesteps+1):
         episode_timesteps += 1
@@ -82,8 +79,11 @@ def train_and_evaluate(config: ml_collections.ConfigDict):
                 0, max_action * config.expl_noise,
                 size=act_dim)).clip(-max_action, max_action)
 
-        next_obs, reward, done, _, _ = env.step(action)
-        replay_buffer.add(obs, action, next_obs, reward, float(done))
+        next_obs, reward, done, info = env.step(action)
+        done_bool = float(done) if "TimeLimit.truncated" not in info else 0
+        # done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+
+        replay_buffer.add(obs, action, next_obs, reward, done_bool)
         obs = next_obs
 
         if t > config.start_timesteps:
@@ -91,17 +91,16 @@ def train_and_evaluate(config: ml_collections.ConfigDict):
             log_info = agent.update(batch)
 
         if done:
-            (obs, _), done = env.reset(), False
+            obs, done = env.reset(), False
             episode_timesteps = 0
 
         if t % config.eval_freq == 0:
-            eval_reward, eval_step, eval_time = eval_policy(agent, eval_env, config.eval_episodes)
+            eval_reward, eval_time = eval_policy(agent, eval_env, config.eval_episodes)
             if t > config.start_timesteps:
                 log_info.update({
                     "step": t,
                     "reward": eval_reward,
                     "eval_time": eval_time,
-                    "eval_step": eval_step,
                     "time": (time.time() - start_time) / 60
                 })
                 logger.info(
@@ -111,13 +110,13 @@ def train_and_evaluate(config: ml_collections.ConfigDict):
                 )
                 logs.append(log_info)
             else:
-                logs.append({"step": t, "reward": eval_reward, "eval_step": eval_step})
+                logs.append({"step": t, "reward": eval_reward})
                 logger.info(
                     f"\n[#Step {t}] eval_reward: {eval_reward:.2f}, eval_time: {eval_time:.2f}\n")
 
         # Save checkpoints
         if t % config.ckpt_freq == 0:
-            agent.save(f"{ckpt_dir}", t // config.ckpt_freq) 
+            agent.save(f"{ckpt_dir}", t // config.ckpt_freq)
 
     log_df = pd.DataFrame(logs)
     log_df.to_csv(f"{config.log_dir}/{config.env_name.lower()}/{exp_name}.csv")
