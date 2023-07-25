361c8d6a445c78f114d5a1a02f8b75610e887f2a
fuyw <royxroy@163.com>
2023-07-25 02:01:28 -0400

    update train utils

--- drq/models.py
+++ drq/models.py
@@ -14,41 +14,11 @@ from flax import linen as nn
 from flax.training import train_state
 from flax.core import FrozenDict
 
+from utils import init_fn, random_crop, batched_random_crop, target_update
 
 ###################
 # Utils Functions #
 ###################
-def init_fn(initializer: str, gain: float = jnp.sqrt(2)):
-    if initializer == "orthogonal":
-        return nn.initializers.orthogonal(gain)
-    elif initializer == "glorot_uniform":
-        return nn.initializers.glorot_uniform()
-    elif initializer == "glorot_normal":
-        return nn.initializers.glorot_normal()
-    return nn.initializers.lecun_normal()
-
-
-def random_crop(key, img, padding):
-    crop_from = jax.random.randint(key, (2,), 0, 2 * padding + 1)
-    crop_from = jnp.concatenate([crop_from, jnp.zeros((2,), dtype=jnp.int32)])
-    padded_img = jnp.pad(
-        img, ((padding, padding), (padding, padding), (0, 0), (0, 0)), mode="edge"
-    )
-    return jax.lax.dynamic_slice(padded_img, crop_from, img.shape)
-
-
-def batched_random_crop(key, imgs, padding=4):
-    keys = jax.random.split(key, imgs.shape[0])
-    return jax.vmap(random_crop, (0, 0, None))(keys, imgs, padding)
-
-
-def target_update(params, target_params, tau: float = 0.005):
-    def _update(param: FrozenDict, target_param: FrozenDict):
-        return tau * param + (1 - tau) * target_param
-    updated_params = jax.tree_util.tree_map(_update, params, target_params)
-    return updated_params
-
-
 class MLP(nn.Module):
     hidden_dims: Sequence[int] = (256, 256)
     init_fn: Callable = nn.initializers.glorot_uniform()
--- drq/utils/train_utils.py
+++ drq/utils/train_utils.py
@@ -1,4 +1,8 @@
 import logging
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+from flax.core import FrozenDict
 
 
 def get_logger(fname: str) -> logging.Logger:
@@ -10,3 +14,34 @@ def get_logger(fname: str) -> logging.Logger:
                         force=True)
     logger = logging.getLogger()
     return logger
+
+
+def init_fn(initializer: str, gain: float = jnp.sqrt(2)):
+    if initializer == "orthogonal":
+        return nn.initializers.orthogonal(gain)
+    elif initializer == "glorot_uniform":
+        return nn.initializers.glorot_uniform()
+    elif initializer == "glorot_normal":
+        return nn.initializers.glorot_normal()
+    return nn.initializers.lecun_normal()
+
+
+def random_crop(key, img, padding):
+    crop_from = jax.random.randint(key, (2,), 0, 2 * padding + 1)
+    crop_from = jnp.concatenate([crop_from, jnp.zeros((2,), dtype=jnp.int32)])
+    padded_img = jnp.pad(
+        img, ((padding, padding), (padding, padding), (0, 0), (0, 0)), mode="edge"
+    )
+    return jax.lax.dynamic_slice(padded_img, crop_from, img.shape)
+
+
+def batched_random_crop(key, imgs, padding=4):
+    keys = jax.random.split(key, imgs.shape[0])
+    return jax.vmap(random_crop, (0, 0, None))(keys, imgs, padding)
+
+
+def target_update(params, target_params, tau: float = 0.005):
+    def _update(param: FrozenDict, target_param: FrozenDict):
+        return tau * param + (1 - tau) * target_param
+    updated_params = jax.tree_util.tree_map(_update, params, target_params)
+    return updated_params
